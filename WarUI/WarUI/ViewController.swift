20fdd11f0776e4bd61232ead00919d1d06221ed5
AsheqBD <asheq@arditesbd.com>
2018-07-11 16:34:36 +0600

    CardGame Deal button activated

--- WarUI/WarUI/ViewController.swift
+++ WarUI/WarUI/ViewController.swift
@@ -10,15 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftRandomNumber = arc4random_uniform(13)+2
+       // print("left random number is:\(leftRandomNumber)")
+        let rightRandomNumber = arc4random_uniform(13)+2
+               // print("left random number is:\(rightRandomNumber)")
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        if leftRandomNumber > rightRandomNumber {
+          //  leftScoreLabel.text = String(leftScore)
+          //  rightScoreLabel.text = String(rightScore)
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftRandomNumber < rightRandomNumber {
+          //  leftScoreLabel.text = String(leftScore)
+          //  rightScoreLabel.text = String(rightScore)
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        else {
+           // leftScoreLabel.text = "Deal "
+           // rightScoreLabel.text = "Again"
+        }
+    }
+    
 
 
 }
